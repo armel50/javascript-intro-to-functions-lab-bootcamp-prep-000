c12c295ed9294ad3848a35a225765f2bfd405249
Cloe Kouadjo <armelfli50@gmail.com>
2019-06-24 04:16:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ function sayHiToGrandma(string){
     return  "I can't hear you!" 
   }else if(string.toUpperCase()){
     return "YES INDEED!"
-  }else if(string ==="")
+  }else if(string ==="I love you, Grandma.")
   
+  return
 }
 
 function sayHiToGrandma(string){}
